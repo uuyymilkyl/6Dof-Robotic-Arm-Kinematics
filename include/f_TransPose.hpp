b042b3ff5db7f799103a4874666ed0ca8e2840ee
黄敏瑜 <huangminyu@mohisttech.com>
2023-12-19 16:42:01 +0800

    1. 更新Matrix类

--- include/f_TransPose.hpp
+++ include/f_TransPose.hpp
@@ -1,3 +1,15 @@
+/**
+*****************************************************************************
+*	@file	f_TransPose.hpp
+*	@brief	姿态表示功能类  transformation of 3dPose description 
+*
+*	@author		HuangMinYu
+*	@date		2023.12.19
+*	@version	V2.0
+*
+*****************************************************************************
+*/
+
 #ifndef _6DOFROBOTICS_TRANSPOSE_HPP_
 #define _6DOFROBOTICS_TRANSPOSE_HPP_
 
--- include/t_Matrix.hpp
+++ include/t_Matrix.hpp
@@ -1,3 +1,15 @@
+/**
+*****************************************************************************
+*	@file	t_Matrix.hpp
+*	@brief	矩阵模板类  template of Matrix calculation
+*
+*	@author		HuangMinYu
+*	@date		2023.12.19
+*	@version	V2.0
+*
+*****************************************************************************
+*/
+
 #ifndef _6DOFROBOTICS_KMATRIX_HPP_
 #define _6DOFROBOTICS_KMATRIX_HPP_
 
@@ -16,6 +28,7 @@ public:
 
 	void operator=(const KMat<T>& _M);                                        ///< 等于 - 矩阵 - 赋值此矩阵
 	void operator=(std::initializer_list<std::initializer_list<T>> _values);  ///< 等于 - 矩阵 - 赋值此二维数组
+	void operator=(std::vector<std::vector<T>> _values);  ///< 等于 - 矩阵 - 赋值此二维数组
 
 	KMat<T> operator+(const KMat<T>& _M);    ///< 加法     
 	KMat<T> operator-(const KMat<T>& _M);    ///< 剪法
@@ -27,13 +40,17 @@ public:
 	void _assign(KMat<T>& _M, int startRow, int endRow, int startCol, int endCol);  //部分赋值 
 	void _append_row(KMat<T>& _M);   //追加为行
 	void _append_col(KMat<T>& _M);   //追加为列
+
+	T _Det(); //求矩阵的行列式
 	
 	KMat<T> _Inv3(); /// <3*3矩阵求逆
 	KMat<T> _Inv4(); /// <4*4矩阵求逆
 
 public:
 
-	T _Determinate(); //求矩阵的行列式
+	
+
+
 private:
 
 	int m_nRows;
@@ -72,6 +89,28 @@ inline void KMat<T>::operator=(const KMat<T>& _M)
 
 template<typename T>
 inline void KMat<T>::operator=(std::initializer_list<std::initializer_list<T>> _values)
+{
+	m_nRows = _values.size();
+	m_nCols = (m_nRows > 0) ? _values.begin()->size() : 0;
+	m_vMat.resize(m_nRows);
+
+	int rowIdx = 0;
+	for (const auto& row : _values)
+	{
+		m_vMat[rowIdx].resize(m_nCols);
+		int colIdx = 0;
+		for (const auto& element : row)
+		{
+			m_vMat[rowIdx][colIdx] = element;
+			colIdx++;
+
+		}
+		rowIdx++;
+	}
+}
+
+template<typename T>
+inline void KMat<T>::operator=(std::vector<std::vector<T>> _values)
 {
 	m_nRows = _values.size();
 	m_nCols = (m_nRows > 0) ? _values.begin()->size() : 0;
@@ -237,17 +276,85 @@ inline void KMat<T>::_append_col(KMat<T>& _M)
 	result._assign(_M, m_nCols+1, NewColsize, 1, m_nRows);
 }
 
+
+
 template<typename T>
 inline KMat<T> KMat<T>::_Inv3()
 {
+	if (m_nCols != 3 && m_nRows != 3)
+	{
+		throw std::invalid_argument("矩阵不为3x3");
+	}
 
-	return KMat<T>();
+	KMat<T> matIn = this->m_vMat;
+
+	KMat<T> matT(m_nRows, m_nCols);
+	
+
+	T determinate = matIn._Det(). //求出行列式的值
+
+	if (determinate == 0)
+	{
+		return matT;
+	}
+
+	T invdet(1.0 / determinate);
+
+	matT.m_vMat[0][0] = (uint_matrix_1 * invdet);
+	matT.m_vMat[0][1] = -(this->m_vMat[0][1] * this->m_vMat[2][2] - this->m_vMat[0][2] * this->m_vMat[2][1]) * invdet;
+	matT.m_vMat[0][2] = (this->m_vMat[0][1] * this->m_vMat[1][2] - this->m_vMat[0][2] * this->m_vMat[1][1]) * invdet;
+	matT.m_vMat[1][0] = -uint_matrix_2 * invdet;
+	matT.m_vMat[1][1] = (this->m_vMat[0][0] * this->m_vMat[2][2] - this->m_vMat[0][2] * this->m_vMat[2][0]) * invdet;
+	matT.m_vMat[1][2] = -(this->m_vMat[0][0] * this->m_vMat[1][2] - this->m_vMat[1][0] * this->m_vMat[0][2]) * invdet;
+	matT.m_vMat[2][0] = (this->m_vMat[1][0] * this->m_vMat[2][1] - this->m_vMat[2][0] * this->m_vMat[1][1]) * invdet;
+	matT.m_vMat[2][1] = -(this->m_vMat[0][0] * this->m_vMat[2][1] - this->m_vMat[2][0] * this->m_vMat[0][1]) * invdet;
+	matT.m_vMat[2][2] = (this->m_vMat[0][0] * this->m_vMat[1][1] - this->m_vMat[1][0] * this->m_vMat[0][1]) * invdet;
+
+	return matT;
 }
 
 template<typename T>
 inline KMat<T> KMat<T>::_Inv4()
 {
 	return KMat<T>();
+}
+
+template<typename T>
+inline T KMat<T>::_Det()
+{
+
+	if (m_nRows != m_nCols) {
+		throw std::invalid_argument("矩阵非方针，不可求行列式.");
+	}
+
+	if (m_nRows == 1) 
+	{
+		return m_vMat[0][0];
+	}
+	else if (m_nRows == 2) 
+	{
+		return m_vMat[0][0] * m_vMat[1][1] - m_vMat[0][1] * m_vMat[1][0];
+	}
+	else {
+		T det = 0;
+		for (int i = 0; i < m_nRows; i++) 
+		{
+			KMat<T> subMat(m_nRows - 1, m_nCols - 1);
+			for (int j = 1; j < m_nRows; j++) {
+				for (int k = 0; k < m_nCols; k++) {
+					if (k < i) {
+						subMat.m_vMat[j - 1][k] = m_vMat[j][k];
+					}
+					else if (k > i) {
+						subMat.m_vMat[j - 1][k - 1] = m_vMat[j][k];
+					}
+				}
+			}
+			T sign = (i % 2 == 0) ? 1 : -1;
+			det += sign * m_vMat[0][i] * subMat._Det();
+		}
+		return det;
+	}
 }
 
 
